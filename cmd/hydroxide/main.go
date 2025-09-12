9f45067157728b25f058c574b7f4ba347bff6e09
Adam Flagg <adam@flagg.cloud>
2025-09-12 15:38:34 -0700

    Remove IMAP and email-related functionality
    - Removed entire IMAP server implementation and /imap directory
    - Removed mbox import/export functionality (/imports and /exports)
    - Removed all IMAP-related command-line flags and commands
    - Simplified 'serve' command to only handle CardDAV
    - Cleaned up dependencies (removed go-imap, go-mbox, go-message)
    - Project now focused solely on CardDAV with future CalDAV support
    - Retained ProtonMail API client, auth, and events for CardDAV/CalDAV

--- cmd/hydroxide/main.go
+++ cmd/hydroxide/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bufio"
-	"bytes"
 	"crypto/tls"
 	"flag"
 	"fmt"
@@ -14,17 +13,12 @@ import (
 
 	"github.com/ProtonMail/go-crypto/openpgp"
 	"github.com/ProtonMail/go-crypto/openpgp/armor"
-	imapserver "github.com/emersion/go-imap/server"
-	"github.com/emersion/go-mbox"
 	"golang.org/x/term"
 
 	"github.com/emersion/hydroxide/auth"
 	"github.com/emersion/hydroxide/carddav"
 	"github.com/emersion/hydroxide/config"
 	"github.com/emersion/hydroxide/events"
-	"github.com/emersion/hydroxide/exports"
-	imapbackend "github.com/emersion/hydroxide/imap"
-	"github.com/emersion/hydroxide/imports"
 	"github.com/emersion/hydroxide/protonmail"
 )
 
@@ -81,24 +75,6 @@ func askBridgePass() (string, error) {
 }
 
 
-func listenAndServeIMAP(addr string, debug bool, authManager *auth.Manager, eventsManager *events.Manager, tlsConfig *tls.Config) error {
-	be := imapbackend.New(authManager, eventsManager)
-	s := imapserver.New(be)
-	s.Addr = addr
-	s.AllowInsecureAuth = tlsConfig == nil
-	s.TLSConfig = tlsConfig
-	if debug {
-		s.Debug = os.Stdout
-	}
-
-	if s.TLSConfig != nil {
-		log.Println("IMAP server listening with TLS on", s.Addr)
-		return s.ListenAndServeTLS()
-	}
-
-	log.Println("IMAP server listening on", s.Addr)
-	return s.ListenAndServe()
-}
 
 
 
@@ -167,23 +143,12 @@ func listenAndServeCardDAV(addr string, authManager *auth.Manager, eventsManager
 	return s.ListenAndServe()
 }
 
-func isMbox(br *bufio.Reader) (bool, error) {
-	prefix := []byte("From ")
-	b, err := br.Peek(len(prefix))
-	if err != nil {
-		return false, err
-	}
-	return bytes.Equal(b, prefix), nil
-}
 
 const usage = `usage: hydroxide [options...] <command>
 Commands:
 	auth <username>		Login to ProtonMail via hydroxide
 	carddav			Run hydroxide as a CardDAV server
 	export-secret-keys <username> Export secret keys
-	imap			Run hydroxide as an IMAP server
-	import-messages <username> [file]	Import messages
-	export-messages [options...] <username>	Export messages
 	sendmail <username> -- <args...>	sendmail(1) interface
 	serve			Run all servers
 	status			View hydroxide status
@@ -195,15 +160,9 @@ Global options:
 		ProtonMail API endpoint
 	-app-version <version>
 		ProtonMail application version
-	-imap-host example.com
-		Allowed IMAP email hostname on which hydroxide listens, defaults to 127.0.0.1
 	-carddav-host example.com
-	-imap-port example.com
-		IMAP port on which hydroxide listens, defaults to 1143
 	-carddav-port example.com
 		CardDAV port on which hydroxide listens, defaults to 8080
-	-disable-imap
-		Disable IMAP for hydroxide serve
 	-disable-carddav
 		Disable CardDAV for hydroxide serve
 	-tls-cert /path/to/cert.pem
@@ -223,9 +182,6 @@ func main() {
 	flag.StringVar(&appVersion, "app-version", defaultAppVersion, "ProtonMail app version")
 
 
-	imapHost := flag.String("imap-host", "127.0.0.1", "Allowed IMAP email hostname on which hydroxide listens, defaults to 127.0.0.1")
-	imapPort := flag.String("imap-port", "1143", "IMAP port on which hydroxide listens, defaults to 1143")
-	disableIMAP := flag.Bool("disable-imap", false, "Disable IMAP for hydroxide serve")
 
 	carddavHost := flag.String("carddav-host", "127.0.0.1", "Allowed CardDAV email hostname on which hydroxide listens, defaults to 127.0.0.1")
 	carddavPort := flag.String("carddav-port", "8080", "CardDAV port on which hydroxide listens, defaults to 8080")
@@ -237,8 +193,6 @@ func main() {
 
 	authCmd := flag.NewFlagSet("auth", flag.ExitOnError)
 	exportSecretKeysCmd := flag.NewFlagSet("export-secret-keys", flag.ExitOnError)
-	importMessagesCmd := flag.NewFlagSet("import-messages", flag.ExitOnError)
-	exportMessagesCmd := flag.NewFlagSet("export-messages", flag.ExitOnError)
 
 	flag.Usage = func() {
 		fmt.Print(usage)
@@ -395,120 +349,22 @@ func main() {
 		if err := wc.Close(); err != nil {
 			log.Fatal(err)
 		}
-	case "import-messages":
-		importMessagesCmd.Parse(flag.Args()[1:])
-		username := importMessagesCmd.Arg(0)
-		archivePath := importMessagesCmd.Arg(1)
-		if username == "" {
-			log.Fatal("usage: hydroxide import-messages <username> [file]")
-		}
-
-		f := os.Stdin
-		if archivePath != "" {
-			f, err = os.Open(archivePath)
-			if err != nil {
-				log.Fatal(err)
-			}
-			defer f.Close()
-		}
-
-		bridgePassword, err := askBridgePass()
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		c, _, _, err := auth.NewManager(newClient).Auth(username, bridgePassword)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		br := bufio.NewReader(f)
-		if ok, err := isMbox(br); err != nil {
-			log.Fatal(err)
-		} else if ok {
-			mr := mbox.NewReader(br)
-			for {
-				r, err := mr.NextMessage()
-				if err == io.EOF {
-					break
-				} else if err != nil {
-					log.Fatal(err)
-				}
-				if err := imports.ImportMessage(c, r); err != nil {
-					log.Fatal(err)
-				}
-			}
-		} else {
-			if err := imports.ImportMessage(c, br); err != nil {
-				log.Fatal(err)
-			}
-		}
-	case "export-messages":
-		// TODO: allow specifying multiple IDs
-		var convID, msgID string
-		exportMessagesCmd.StringVar(&convID, "conversation-id", "", "conversation ID")
-		exportMessagesCmd.StringVar(&msgID, "message-id", "", "message ID")
-		exportMessagesCmd.Parse(flag.Args()[1:])
-		username := exportMessagesCmd.Arg(0)
-		if (convID == "" && msgID == "") || username == "" {
-			log.Fatal("usage: hydroxide export-messages [-conversation-id <id>] [-message-id <id>] <username>")
-		}
-
-		bridgePassword, err := askBridgePass()
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		c, privateKeys, _, err := auth.NewManager(newClient).Auth(username, bridgePassword)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		mboxWriter := mbox.NewWriter(os.Stdout)
-
-		if convID != "" {
-			if err := exports.ExportConversationMbox(c, privateKeys, mboxWriter, convID); err != nil {
-				log.Fatal(err)
-			}
-		}
-		if msgID != "" {
-			if err := exports.ExportMessageMbox(c, privateKeys, mboxWriter, msgID); err != nil {
-				log.Fatal(err)
-			}
-		}
-
-		if err := mboxWriter.Close(); err != nil {
-			log.Fatal(err)
-		}
-	case "imap":
-		addr := *imapHost + ":" + *imapPort
-		authManager := auth.NewManager(newClient)
-		eventsManager := events.NewManager()
-		log.Fatal(listenAndServeIMAP(addr, debug, authManager, eventsManager, tlsConfig))
 	case "carddav":
 		addr := *carddavHost + ":" + *carddavPort
 		authManager := auth.NewManager(newClient)
 		eventsManager := events.NewManager()
 		log.Fatal(listenAndServeCardDAV(addr, authManager, eventsManager, tlsConfig))
 	case "serve":
-		imapAddr := *imapHost + ":" + *imapPort
 		carddavAddr := *carddavHost + ":" + *carddavPort
 
 		authManager := auth.NewManager(newClient)
 		eventsManager := events.NewManager()
 
-		done := make(chan error, 2)
-		if !*disableIMAP {
-			go func() {
-				done <- listenAndServeIMAP(imapAddr, debug, authManager, eventsManager, tlsConfig)
-			}()
-		}
 		if !*disableCardDAV {
-			go func() {
-				done <- listenAndServeCardDAV(carddavAddr, authManager, eventsManager, tlsConfig)
-			}()
+			log.Fatal(listenAndServeCardDAV(carddavAddr, authManager, eventsManager, tlsConfig))
+		} else {
+			log.Fatal("CardDAV is disabled - no services to run")
 		}
-		log.Fatal(<-done)
 	case "sendmail":
 		// SendMail functionality removed - SMTP support discontinued
 		log.Fatal("sendmail command is no longer supported - SMTP functionality has been removed")
